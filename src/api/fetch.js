3842c218bbd280144a8ac398a500fb890b7157b1
Erick Tolentino <ericktolentino@pursuit.org>
2023-09-13 16:20:58 -0400

    added api

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,5 +1,25 @@
-const _BASE_URL = `www.themealdb.com/api/json/v1/1/search.php?`
+const _BASE_URL = "https://www.themealdb.com/api/json/v1/1/";
 
 export function getAllRecipes() {
-    return fetch (`${_BASE_URL}f=a`)
+  const endpoint = "search.php?f=a";
+  const url = `${_BASE_URL}${endpoint}`;
+
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      if (data.meals && Array.isArray(data.meals)) {
+        return data.meals;
+      } else {
+        throw new Error("Invalid API response: 'meals' property not found.");
+      }
+    })
+    .catch((error) => {
+      console.error("Fetch error:", error);
+      throw error;
+    });
 }
