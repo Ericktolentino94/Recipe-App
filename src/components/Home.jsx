54eb539011c1ecc0e6fb904729ad92a93444d2a4
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-09-13 20:00:02 -0400

    installs react-router-dom and imports link to home jsx component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { getAllRecipes } from "../api/fetch";
+import {Link} from 'react-router-dom'
 
 const Home = () => {
   const [recipes, setRecipes] = useState([]);
