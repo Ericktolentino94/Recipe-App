ba1cdfc41991eaae92d63c30a1724f487c845767
Erick Tolentino <ericktolentino@pursuit.org>
2023-09-19 16:50:59 -0400

    added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,6 +5,7 @@ import "../components/Nav.css"
 const Nav = () => {
     return (
         <nav className="nav-bar">
+            <div>
       <img
         className="header-logo"
         src="https://b2066476.smushcdn.com/2066476/wp-content/uploads/2018/10/25698-TSET-19-04-SYF-Website-Refresh_Header_5FoodGroups_F.jpg?lossy=1&strip=1&webp=1"
@@ -12,13 +13,16 @@ const Nav = () => {
         width="300px"
         alt="Recipe Logo"
       />
-      <h1>Produce Saver</h1>
+      </div>
+      <div className='nav-cont'>
+      <h1 className='navhead'>The Recipe App</h1>
 
       <ul>
         <li className='home'>
           <Link to="/">Home</Link>
         </li>
       </ul>
+      </div>
     </nav>
     );
 }
